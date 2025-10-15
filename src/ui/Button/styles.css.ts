f7fc0a1924beb6d6b2a1880f4b6bd19471e68653
Evgenii Troinov <troinof@gmail.com>
2025-10-15 14:30:00 +0200

    main: Updated ui

--- src/ui/Button/styles.css.ts
+++ src/ui/Button/styles.css.ts
@@ -2,16 +2,25 @@ import { style } from "@vanilla-extract/css";
 
 export const container = style({
   display: "flex",
+  backgroundColor: "#f8f8f9",
+  borderRadius: "8px",
 });
 
 export const input = style({
   width: "40px",
-  height: "28px",
+  height: "32px",
   border: "none",
+  outline: "none",
+  textAlign: "center",
+  backgroundColor: "transparent",
 });
 
 export const btn = style({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
   width: "32px",
   height: "32px",
   border: "none",
+  backgroundColor: "transparent",
 });
