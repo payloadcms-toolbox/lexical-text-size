95357e4da7c12be2c995f62c3ff18b8fe6b62cac
Evgenii Troinov <troinof@gmail.com>
2025-10-30 13:36:03 +0100

    main: Fixed lint

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,31 +2,31 @@ import { vanillaExtractPlugin } from "@vanilla-extract/esbuild-plugin";
 import { defineConfig, type Options } from "tsup";
 
 const commonConfig: Partial<Options> = {
-  format: ["esm"],
-  dts: true,
-  sourcemap: false,
-  minify: true,
-  external: ["react", "react-dom", "react/jsx-runtime", /^@payloadcms\/.*/],
-  esbuildPlugins: [vanillaExtractPlugin()],
-  injectStyle: false,
+	format: ["esm"],
+	dts: true,
+	sourcemap: false,
+	minify: true,
+	external: ["react", "react-dom", "react/jsx-runtime", /^@payloadcms\/.*/],
+	esbuildPlugins: [vanillaExtractPlugin()],
+	injectStyle: false,
 };
 
 export default defineConfig([
-  {
-    ...commonConfig,
-    entry: {
-      index: "src/index.ts",
-    },
-    clean: true,
-  },
-  {
-    ...commonConfig,
-    entry: {
-      "feature.client": "src/feature.client.tsx",
-    },
-    clean: false,
-    banner: {
-      js: '"use client";',
-    },
-  },
+	{
+		...commonConfig,
+		entry: {
+			index: "src/index.ts",
+		},
+		clean: true,
+	},
+	{
+		...commonConfig,
+		entry: {
+			"feature.client": "src/feature.client.tsx",
+		},
+		clean: false,
+		banner: {
+			js: '"use client";',
+		},
+	},
 ]);
